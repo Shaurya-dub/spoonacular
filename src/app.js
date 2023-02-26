5337e3260e26bdcc680cebad081a9a55c78359fa
Shaurya-dub <shauryadubey@hotmail.ca>
2023-02-26 17:59:38 -0500

    basic pagination done

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ const recipeForm = document.querySelector(".recipeSearchForm");
 const recipeList = document.querySelector(".recipeList");
 const recipeSearchInput = document.querySelector(".recipeSearchInput");
 const paginationNumbers = document.querySelector("#paginationNumbers");
-const nextButton = document.querySelector("nextButton");
-const prevButton = document.querySelector("prevButton");
+const nextButton = document.querySelector("#nextButton");
+const prevButton = document.querySelector("#prevButton");
 const paginationLimit = 5;
-let currentPage;
+let currentPage = 1;
+let pageCount;
 let cuisineFilterArray = [];
 recipeForm.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -23,17 +24,46 @@ recipeForm.addEventListener("submit", async (e) => {
       },
     }
   );
-  if (request) {
-    console.log("req", request);
+  if (request.data.results.length > 0) {
+    const recipeResults = request.data.results;
+    console.log("req", recipeResults);
     recipeList.innerHTML = "";
-    request.data.results.forEach((rec) => {
+    pageCount = Math.ceil(recipeResults.length / paginationLimit);
+    // let currentPage
+    getPaginationNumbers(pageCount);
+    recipeResults.forEach((rec) => {
       recipeDisplayFunction(rec);
+    });
+    const appendedListItems = document.querySelectorAll(".recipeCard");
+    setCurrentPage(1, appendedListItems);
+    prevButton.addEventListener("click", () => {
+      setCurrentPage(currentPage - 1, appendedListItems);
+    });
+    nextButton.addEventListener("click", () => {
+      setCurrentPage(currentPage + 1, appendedListItems);
+    });
+    document.querySelectorAll(".paginationNumber").forEach((button) => {
+      const pageIndex = Number(button.getAttribute("page-index"));
+      if (pageIndex) {
+        button.addEventListener("click", () => {
+          setCurrentPage(pageIndex, appendedListItems);
+        });
+      }
     });
   } else {
     console.error("empty response");
   }
   //   const answer = await request.json();
 });
+
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement("button");
+  pageNumber.className = "paginationNumber";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + index);
+  paginationNumbers.appendChild(pageNumber);
+};
 
 const recipeDisplayFunction = (rec) => {
   const recipeListItem = document.createElement("li");
@@ -43,6 +73,60 @@ const recipeDisplayFunction = (rec) => {
 <div class="recipeImageHolder"><img src="${rec.image}" alt="picture of ${rec.title}"></div>
 <h2>${rec.title}</h2>`;
   recipeList.appendChild(recipeListItem);
+};
+
+const setCurrentPage = (pageNum, recipes) => {
+  currentPage = pageNum;
+  handleActivePageNumber();
+  handlePageButtonsStatus();
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+  recipes.forEach((item, index) => {
+    // console.log("in loop", item, index);
+    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hidden");
+    }
+  });
+};
+
+const getPaginationNumbers = (numOfPages) => {
+  paginationNumbers.innerHTML = "";
+  for (let i = 1; i <= numOfPages; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".paginationNumber").forEach((button) => {
+    button.classList.remove("active");
+
+    const activePage = Number(button.getAttribute("page-index"));
+    if (activePage == currentPage) {
+      button.classList.add("active");
+    }
+  });
+};
+
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
 };
 
 function checkboxSelect(e) {
